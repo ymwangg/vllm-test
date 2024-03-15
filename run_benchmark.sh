852b2456e617dc0c79f15e8d91de15878e6ff578
Yanming Wang <yanmwang@amazon.com>
2024-03-15 22:12:15 +0000

    Remove max steps in run_bnechmark.sh

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,4 +2,4 @@ target=/home/ubuntu/models/CodeLlama-34b-Python-hf
 draft=/home/ubuntu/models/TinyLlama-1.1B-Chat-v1.0
 python benchmark_speculate.py --batch-size 32 --run-human-eval --tp-size 2 \
   --model $target --draft-model $draft --max-tokens 512 \
-  --temperature 0.0 --top-p 1.0 --frequency-penalty 0.1 --block-size 16 --max-steps 3 --use-speculate
+  --temperature 0.0 --top-p 1.0 --frequency-penalty 0.1 --block-size 16 --use-speculate
