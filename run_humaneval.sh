19b9a806ca30fcce89879abfece0d93ad9a4d398
Yanming Wang <yanmwang@amazon.com>
2024-03-29 17:25:39 +0000

    Add run_humaneval.sh

---
+++ run_humaneval.sh
@@ -0,0 +1,5 @@
+target=/home/ubuntu/models/CodeLlama-34b-Python-hf
+draft=/home/ubuntu/models/TinyLlama-1.1B-Chat-v1.0
+python benchmark_speculate.py --batch-size 32 --run-human-eval --tp-size 4 \
+  --model $target --draft-model $draft --max-tokens 512 \
+  --temperature 0.0 --top-p 1.0 --frequency-penalty 0.1 --block-size 16 --use-speculate --speculate-length 5
