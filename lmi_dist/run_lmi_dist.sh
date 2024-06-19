611fd1dfcf223bf15738cf7e988d79c8956f7b16
Yanming Wang <yanmwang@amazon.com>
2024-06-19 06:40:00 +0000

    Enhance sh script

--- lmi_dist/run_lmi_dist.sh
+++ lmi_dist/run_lmi_dist.sh
@@ -5,20 +5,26 @@ max_tokens=256
 max_steps=10
 gpu_util=0.8
 
-CMD="mpirun --allow-run-as-root --bind-to none --mca btl_vader_single_copy_mechanism none --tag-output -x FI_PROVIDER=efa -x RDMAV_FORK_SAFE=1 -x FI_EFA_USE_DEVICE_RDMA=1 -x LD_LIBRARY_PATH -x PYTHONPATH -x MKL_DYNAMIC=FALSE"
-
 dataset=../dataset/humaneval.jsonl
 target=/home/ubuntu/models/Llama-3-70b-instruct-hf
 draft=/home/ubuntu/models/Llama-3-8b-instruct-hf
 
+MPIRUN="mpirun --allow-run-as-root --bind-to none --mca btl_vader_single_copy_mechanism none --tag-output -x FI_PROVIDER=efa -x RDMAV_FORK_SAFE=1 -x FI_EFA_USE_DEVICE_RDMA=1 -x LD_LIBRARY_PATH -x PYTHONPATH -x MKL_DYNAMIC=FALSE"
+
+CMD="$MPIRUN -n $tp python  benchmark_speculate.py --tp-size $tp --model $target --draft-model $draft --batch-size $bs --dataset $dataset --max-steps $max_steps  --max-tokens $max_tokens --draft-tp-size $draft_tp  --use-speculate --speculate-length 5 --gpu-memory-utilization $gpu_util"
+
 # no vmmtensor + no custom-ar
-$CMD -n $tp python  benchmark_speculate.py --tp-size $tp --model $target --draft-model $draft --run-human-eval --batch-size $bs --dataset $dataset --max-steps $max_steps  --max-tokens $max_tokens --draft-tp-size $draft_tp  --use-speculate --speculate-length 5 --gpu-memory-utilization $gpu_util --disable-custom-all-reduce
+$CMD --disable-custom-all-reduce > 0.txt 2>&1
+grep "Throughput" 0.txt
 
 # vmmtensor + custom-ar
-$CMD -n $tp python  benchmark_speculate.py --tp-size $tp --model $target --draft-model $draft --run-human-eval --batch-size $bs --dataset $dataset --max-steps $max_steps  --max-tokens $max_tokens --draft-tp-size $draft_tp  --use-speculate --speculate-length 5 --gpu-memory-utilization $gpu_util --disable-custom-all-reduce --vmmtensor-kv-cache
+$CMD --disable-custom-all-reduce --vmmtensor-kv-cache > 1.txt 2>&1
+grep "Throughput" 1.txt
 
 # no vmmtensor + custom-ar
-$CMD -n $tp python  benchmark_speculate.py --tp-size $tp --model $target --draft-model $draft --run-human-eval --batch-size $bs --dataset $dataset --max-steps $max_steps  --max-tokens $max_tokens --draft-tp-size $draft_tp  --use-speculate --speculate-length 5 --gpu-memory-utilization $gpu_util
+$CMD > 2.txt 2>&1
+grep "Throughput" 2.txt
 
 # vmmtensor + custom-ar
-$CMD -n $tp python  benchmark_speculate.py --tp-size $tp --model $target --draft-model $draft --run-human-eval --batch-size $bs --dataset $dataset --max-steps $max_steps  --max-tokens $max_tokens --draft-tp-size $draft_tp  --use-speculate --speculate-length 5 --gpu-memory-utilization $gpu_util --vmmtensor-kv-cache
+$CMD --vmmtensor-kv-cache > 3.txt 2>&1
+grep "Throughput" 3.txt
