8f62c38b58ff6be0d7d2ce25fa2ee8d7679cf14f
Yanming Wang <yanmwang@amazon.com>
2024-06-19 17:32:58 +0000

    Rename models

--- lmi_dist/run_lmi_dist.sh
+++ lmi_dist/run_lmi_dist.sh
@@ -6,8 +6,8 @@ max_steps=10
 gpu_util=0.8
 
 dataset=../dataset/humaneval.jsonl
-target=/home/ubuntu/models/Llama-3-70b-instruct-hf
-draft=/home/ubuntu/models/Llama-3-8b-instruct-hf
+target=/home/ubuntu/models/Meta-Llama-3-70B-Instruct
+draft=/home/ubuntu/models/Meta-Llama-3-8B-Instruct
 
 MPIRUN="mpirun --allow-run-as-root --bind-to none --mca btl_vader_single_copy_mechanism none --tag-output -x FI_PROVIDER=efa -x RDMAV_FORK_SAFE=1 -x FI_EFA_USE_DEVICE_RDMA=1 -x LD_LIBRARY_PATH -x PYTHONPATH -x MKL_DYNAMIC=FALSE"
 
