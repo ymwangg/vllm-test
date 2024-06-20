dd41e3e8c06ea06c75359df3ece7b6a1e43204cd
Yanming Wang <yanmwang@amazon.com>
2024-06-20 04:17:50 +0000

    Add chat-template example

---
+++ chat_template/apply_template.py
@@ -0,0 +1,9 @@
+from fastchat.model.model_adapter import get_conversation_template
+
+for model in ['llama-2','llama-3']:
+    conv = get_conversation_template(model)
+    conv.set_system_message("{system_message}")
+    conv.append_message(conv.roles[0], "{user_prompt}")
+    conv.append_message(conv.roles[1], None)
+    print(model)
+    print(f"{conv.get_prompt()!r}")
