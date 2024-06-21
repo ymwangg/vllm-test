d32e213eb1538754b965b3c4779ae3c813399068
Yanming Wang <yanmwang@amazon.com>
2024-06-21 23:56:39 +0000

    Update lmi_dist script

--- lmi_dist/run_lmi_dist.sh
+++ lmi_dist/run_lmi_dist.sh
@@ -11,20 +11,8 @@ draft=/home/ubuntu/models/Meta-Llama-3-8B-Instruct
 
 MPIRUN="mpirun --allow-run-as-root --bind-to none --mca btl_vader_single_copy_mechanism none --tag-output -x FI_PROVIDER=efa -x RDMAV_FORK_SAFE=1 -x FI_EFA_USE_DEVICE_RDMA=1 -x LD_LIBRARY_PATH -x PYTHONPATH -x MKL_DYNAMIC=FALSE"
 
-CMD="$MPIRUN -n $tp python  benchmark_speculate.py --tp-size $tp --model $target --draft-model $draft --batch-size $bs --dataset $dataset --max-steps $max_steps  --max-tokens $max_tokens --draft-tp-size $draft_tp  --use-speculate --speculate-length 5 --gpu-memory-utilization $gpu_util"
+CMD="$MPIRUN -n $tp python  benchmark_speculate.py --tp-size $tp --model $target --draft-model $draft --batch-size $bs --dataset $dataset --max-steps $max_steps  --max-tokens $max_tokens --draft-tp-size $draft_tp  --gpu-memory-utilization $gpu_util"
 
-# no vmmtensor + no custom-ar
-$CMD --disable-custom-all-reduce > 0.txt 2>&1
-grep "Throughput" 0.txt
+$CMD --use-speculate --speculate-length 5
 
-# vmmtensor + custom-ar
-$CMD --disable-custom-all-reduce --vmmtensor-kv-cache > 1.txt 2>&1
-grep "Throughput" 1.txt
-
-# no vmmtensor + custom-ar
-$CMD > 2.txt 2>&1
-grep "Throughput" 2.txt
-
-# vmmtensor + custom-ar
-$CMD --vmmtensor-kv-cache > 3.txt 2>&1
-grep "Throughput" 3.txt
+$CMD
