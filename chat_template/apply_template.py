d9da5b28e30f62f89e4d9de46fbdd41c94012f7b
Yanming Wang <yanmwang@amazon.com>
2024-08-21 23:57:20 +0000

    Bump to 0.5.4

--- chat_template/apply_template.py
+++ chat_template/apply_template.py
@@ -4,6 +4,6 @@ for model in ['llama-2','llama-3']:
     conv = get_conversation_template(model)
     conv.set_system_message("{system_message}")
     conv.append_message(conv.roles[0], "{user_prompt}")
-    conv.append_message(conv.roles[1], None)
+    conv.append_message(conv.roles[1], "{response}")
     print(model)
     print(f"{conv.get_prompt()!r}")
